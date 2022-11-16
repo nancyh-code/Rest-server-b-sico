ff2f3ece75d742a23fe76bb9074eb8f047ee4cf6
nancy <hernandezyanez@hotmail.com>
2022-11-16 17:50:38 -0300

    path productos

--- models/server.js
+++ models/server.js
@@ -11,6 +11,7 @@ class Server {
     this.paths = {
       auth: '/api/auth',
       categorias: '/api/categorias',
+      productos: '/api/productos',
       usuarios: '/api/usuarios',
     };
 
@@ -42,6 +43,7 @@ class Server {
   routes() {
     this.app.use(this.paths.auth, require('../routes/auth'));
     this.app.use(this.paths.categorias, require('../routes/categorias'));
+    this.app.use(this.paths.productos, require('../routes/productos'));
     this.app.use(this.paths.usuarios, require('../routes/usuarios'));
   }
 
