f540520a84415d4ca7f10993e31be7a5de733088
nancy <hernandezyanez@hotmail.com>
2022-11-17 16:23:22 -0300

    busquedas por usuario, categoria y producto

---
+++ controllers/buscar.js
@@ -0,0 +1,115 @@
+const { response } = require('express');
+const { isValidObjectId } = require('mongoose');
+const { Usuario, Categoria, Producto } = require('../models');
+
+const coleccionesPermitidas = ['categorias', 'productos', 'roles', 'usuarios'];
+
+//Búsqueda por usuarios
+const buscarUsuarios = async (termino = '', res = response) => {
+  const esMongoID = isValidObjectId(termino); //True
+
+  if (esMongoID) {
+    const usuario = await Usuario.findById(termino);
+    return res.json({
+      results: usuario ? [usuario] : [],
+    });
+  }
+
+  const regex = new RegExp(termino, 'i');
+
+  const usuarios = await Usuario.find({
+    $or: [
+      {
+        nombre: regex,
+      },
+      {
+        correo: regex,
+      },
+    ],
+    $and: [{ estado: true }],
+  });
+
+  res.json({
+    results: usuarios,
+  });
+};
+
+//Búsqueda por categorías
+const buscarCategorias = async (termino = '', res = response) => {
+  const esMongoID = isValidObjectId(termino); //True
+
+  if (esMongoID) {
+    const categoria = await Categoria.findById(termino);
+    return res.json({
+      results: categoria ? [categoria] : [],
+    });
+  }
+
+  const regex = new RegExp(termino, 'i');
+
+  const categorias = await Categoria.find({
+    nombre: regex,
+    estado: true,
+  });
+
+  res.json({
+    results: categorias,
+  });
+};
+
+//Búsqueda por productos
+const buscarProductos = async (termino = '', res = response) => {
+  const esMongoID = isValidObjectId(termino); //True
+
+  if (esMongoID) {
+    const producto = await Producto.findById(termino).populate(
+      'categoria',
+      'nombre'
+    );
+    return res.json({
+      results: producto ? [producto] : [],
+    });
+  }
+
+  const regex = new RegExp(termino, 'i');
+
+  const productos = await Producto.find({
+    nombre: regex,
+    estado: true,
+  }).populate('categoria', 'nombre');
+
+  res.json({
+    results: productos,
+  });
+};
+
+const buscar = (req, res = response) => {
+  const { coleccion, termino } = req.params;
+
+  if (!coleccionesPermitidas.includes(coleccion)) {
+    return res.status(400).json({
+      msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`,
+    });
+  }
+
+  switch (coleccion) {
+    case 'categorias':
+      buscarCategorias(termino, res);
+      break;
+    case 'productos':
+      buscarProductos(termino, res);
+      break;
+    case 'usuarios':
+      buscarUsuarios(termino, res);
+      break;
+
+    default:
+      res.status(500).json({
+        msg: 'Se olvidó hacer esta búsqueda',
+      });
+  }
+};
+
+module.exports = {
+  buscar,
+};
